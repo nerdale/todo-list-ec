d0e45aa84e2c75a252fce4461bacfc93f2ac46f9
nerdale <alexandra.neira@gmail.com>
2017-07-06 20:32:11 -0400

    agregando comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,21 @@
+
 class Model{
+	//constructor del modelo con un arreglo vacío, al que pretendiamos pushearle las tareas, no alcanzamos :(
 	constructor(array){
 		this.todoList = [];
 	}
-	delete(){};
 }
-
+// se crea la clase controller en la cual se llama al modelo y la vista
 class Controller{
+	// el constructor llama como propiedades la clase model y clase view
 	constructor(){
 		this.model = new Model();
 		this.view = new View(this);
 	}
+	// se inicializa metodo del controlador sobre el input que llama al metodo de la vista (metodo add).
 	initKeys(){
 		var self = this;
 		var input = $('#input_text');
-
 		window.onkeypress = function(e){
 			if(e.key ==="Enter"){
 				self.view.add();
@@ -23,13 +25,15 @@ class Controller{
 	}
 }
 
-
+// en la clase vista instanciamos el controlador e inicializamos los metodos dentro de este
 class View{
 	constructor(controller){
 		this.controller = controller;
+		//inicializo los metodos de borrar y completar. Lo habíamos llamado en el controlador pero no nos funcionó y lo inicializamos en la vista, Ricardo nos dijo xD.
 		this.initDelete();
 		this.initCompleted();
 	}
+	//metodo agregar que valida si el input está vacío de lo contrario crea los elementos necesarios
 	add(){
 		var task = $('#input_text').val();
 		if(task == ''){
@@ -39,15 +43,14 @@ class View{
 			$('#input_text').val('');
 		}
 	}
+	//metodo que borra la tarea. Utiliza on vinculando el evento click al document y que cuando le haga click al elemento del document ocurra lo indicado en la función
 	initDelete(){
-		
 		$(document).on('click', '.delete', function(){
 			console.log($('.delete').parent());
 			$(this).parent().remove();
-			//$('.delete').find('div').remove();
 		})
 	}
-
+	//metodo que completa la tarea. Utiliza on vinculando el evento click al document y que cuando le haga click al elemento del document ocurra lo indicado en la función
 	initCompleted(){
 		$(document).on('click', '.done', function(){
 			$('#nuevaTarea').addClass('.color');
@@ -55,12 +58,6 @@ class View{
 		})
 	}
 }
-
+//se instancia la clase controlador y se inicializa
 var c = new Controller;
 c.initKeys();
-c.erase();
-c.complet();
-
-
-
-
